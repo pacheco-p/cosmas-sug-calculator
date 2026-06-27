36c62a88d048f88f6f64642e114e03d0d3fa9e09
pacheco-p <pedroprince53@gmail.com>
2026-06-27 23:40:43 +0100

    sug_db.py

--- sug_db.py
+++ sug_db.py
@@ -4,13 +4,8 @@ import bcrypt
 def init_db():
     conn = sqlite3.connect("sug_portal.db")
     c = conn.cursor()
+    # Storing student data
     c.execute('''CREATE TABLE IF NOT EXISTS students 
                  (username TEXT PRIMARY KEY, password TEXT, total_units REAL, total_points REAL)''')
     conn.commit()
     conn.close()
-
-def hash_password(password):
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-def check_password(password, hashed):
-    return bcrypt.checkpw(password.encode('utf-8'), hashed)
